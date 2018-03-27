c161116fba495797d705af0ce59ddd7c7213f849
HansGerber <hans.gerber7mail.com>
2018-03-27 21:13:52 +0200

    Create seperate configs for dev and prod

---
+++ conf_prod.php
@@ -0,0 +1,12 @@
+<?php
+
+	$_global_conf = array (
+		"sql" => array (
+			"server" => "localhost",
+			"user" => "dazo_angular",
+			"password" => "123456",
+			"db" => "dazo_angular_de"
+		),
+	);
+
+?>
