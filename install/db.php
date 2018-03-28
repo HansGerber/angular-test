c804bab3d9ccf8e236f19ca2ba475021d0255e79
HansGerber <hans.gerber7mail.com>
2018-03-28 15:35:24 +0200

    Adjust install script (only (re)install tables; add test user entry)

--- install/db.php
+++ install/db.php
@@ -2,7 +2,7 @@
 
 	require_once "../conf.php";
 	
-        $deleteExistingDatabase = @$_GET["new"];
+        $new = @$_GET["new"];
         
 	$sql = $_global_conf["sql"];
 	$db = $sql["db"];
@@ -19,31 +19,29 @@
 		echo "<p>Connected</p>";
 	}
 	
-        if($deleteExistingDatabase){
-            if(!$c->query("drop database $db")){
-                    echo "<p>Failed to delete database $db : ".$c->error."</p>";
-            } else {
-                    echo "<p>Existing database $db deleted.</p>";
-            }
-        }
-        
-	if(!$c->query("create database if not exists $db")){
-		echo "<p>Failed to create database $db : ".$c->error."</p>";
-	} else {
-		echo "<p>Database $db created.</p>";
+	if($new){
+		foreach($sql["tables"] as $table){
+			if(!$c->query("drop table if exists $db.$table")){
+				echo "<p>Failed to remove table $table</p>";
+			} else {
+				echo "<p>Table $table removed</p>";
+			}
+		}
 	}
 	
 	if(!$c->query("create table $db.users (
 		id int(5) not null primary key auto_increment,
-		cid int(8) not null,
+		cid int(8) not null unique,
 		name varchar(50) not null,
 		email varchar(255) not null,
 		password varchar(32) not null,
-		phone varchar(255) not null
+		phone varchar(50) not null,
+		joindate date not null,
+		disabled tinyint(1) not null
 	)")){
 		echo "<p>Failed to create table contact : ".$c->error."</p>";
 	} else {
-		echo "<p>Table users created.</p>";
+		echo "<p>Table users created</p>";
 	}
 	
 	if(!$c->query("create table $db.contact (
@@ -55,7 +53,7 @@
 	)")){
 		echo "<p>Failed to create table contact : ".$c->error."</p>";
 	} else {
-		echo "<p>Table contact created.</p>";
+		echo "<p>Table contact created</p>";
 	}
         
 	if(!$c->query("create table $db.tickets (
@@ -69,8 +67,28 @@
 	)")){
 		echo "<p>Failed to create table tickets : ".$c->error."</p>";
 	} else {
-		echo "<p>Table tickets created.</p>";
+		echo "<p>Table tickets created</p>";
 	}
 
+	// ADDING TEST ENTRIES
+	
+	// - Test user
+	
+	if(!$c->query("insert into $db.users values (
+		'',
+		12345678,
+		'Hans Gerber',
+		'hans.gerber7@gmail.com',
+		'".md5("123456")."',
+		'(040) 12345678',
+		'".date("Y-m-d")."',
+		0
+		
+	)")){
+		echo "<p>Failed to add test user : ".$c->error."</p>";
+	} else {
+		echo "<p>Test user added</p>";
+	}
+	
 	$c->close();
 ?>
